9a396f4976b6a39d0213fb534e21b16eb7a76419
ricardo55 <pegassoric@hotmail.com>
2022-05-05 00:13:03 -0500

    Creación de una clase y modularización

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,13 @@
+const MissionCommander = require('./app/missionCommander');
+
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
+const rica = new MissionCommander("Ricardo")
+console.log(rica.name)
+
+const osiris = new MissionCommander("Osiris")
+console.log(osiris.name)
+
+const firus = new MissionCommander("Firulais")
+console.log(firus.name)

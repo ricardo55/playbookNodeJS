69884a0671ce4098f9d87bfa04d374eb41d1092f
ricardo55 <pegassoric@hotmail.com>
2022-05-07 16:14:36 -0500

    2. Valida que un `payload` contenga en alguna de las llaves `username`, `name`, o `id` un valor en `null`. Si hay un valor `null` regresa un objeto con la llave `error` y que indique el texto: `necesitan tener un valor válido`.

--- weekly_mission_3/twitter/app/views/UserView.js
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -6,9 +6,13 @@ class UserView{
     if(payload === null){
       console.log("Error es null")
       return {error: "El payload no existe."}
+
     } else if(typeof payload.username === 'string' && typeof payload.name === 'string' && typeof payload.id === 'number') {
-      return UserService.create(payload.id, payload.username, payload.name)
+
+      return UserService.create(payload.id, payload.username, payload.name);
+
     } else{
+        
       return {error: "Error, las propiedades del payload necesitan tener un valor válido"}
     }
   }

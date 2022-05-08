128d9fa27456ba70cef98f3881ed6b7563bdfb70
ricardo55 <pegassoric@hotmail.com>
2022-05-08 14:13:53 -0500

    Cree un endpoint para eliminar un explorer con el metodo DELETE

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -56,6 +56,14 @@ app.put('/v1/explorers/:id', (req, res) => {
     res.status(200).json({ message: "Updated!" })
 });
 
+// DELETE Crea un endpoint para eliminar un explorer
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers DELETE request ${new Date()}`);
+    console.log(`Delete explorer with id ${req.params.id}`);
+    const requestBody = req.body // Parámetros de un cliente
+    res.status(200).json({ message: "Deleted" });
+});
+
 
 // Con esto inicializamos esta app
 app.listen(port, () => {

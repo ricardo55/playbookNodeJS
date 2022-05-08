da0b76fe692a356ff64be22bbb14c3dda5b20227
ricardo55 <pegassoric@hotmail.com>
2022-05-08 14:08:42 -0500

    Cree un endpoint que se encargue de crear un explorer con el metodo POST

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -24,26 +24,32 @@ const port = 3000
 app.get('/v1/explorers', (req, res) => {
     console.log(`Api Explorers GET ALL requests ${new Date()}`)
     console.log(`Getting explorer with id ${req.params.id}`)
-    const explorer1 = {id: 1, name: "Ricardo"}
-    const explorer2 = {id: 2, name: "Osiris"}
-    const explorer3 = {id: 3, name: "Fany"}
-    const explorer4 = {id: 4, name: "Napoleon"}
+    const explorer1 = { id: 1, name: "Ricardo" }
+    const explorer2 = { id: 2, name: "Osiris" }
+    const explorer3 = { id: 3, name: "Fany" }
+    const explorer4 = { id: 4, name: "Napoleon" }
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
-  });
+});
 
 
 // Creando un explorer que se regrese mediante el ID
 app.get('/v1/explorers/:id', (req, res) => {
     console.log(`Api Explorers GET request ${new Date()}`)
     console.log(`Getting explorer with id ${req.params.id}`)
-    const explorer = {id: 1, name: "Ricardo"}
+    const explorer = { id: 1, name: "Ricardo" }
     res.status(200).json(explorer)
-  });
+});
 
+// POST Crea un endpoint que se encargue de crear un explorer
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body // Parámetros de un cliente
+    res.status(201).json({ message: "Created" })
+});
 
 
 // Con esto inicializamos esta app
 app.listen(port, () => {
- console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 });

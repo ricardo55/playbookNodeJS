e1b968a24d4695cbf4b0fd5b43d5ebf007460249
ricardo55 <pegassoric@hotmail.com>
2022-07-01 15:47:01 -0500

    6. Metodo que regresa la cantidad de explorers en la misión enviada.

--- weekly_mission_4/refactoring/lib/controllers/ExplorerController.js
+++ weekly_mission_4/refactoring/lib/controllers/ExplorerController.js
@@ -5,15 +5,25 @@ const FizzbuzzService = require("../../lib/services/FizzbuzzService");
 
 class ExplorerController {
 
+    // método para obtener la lista de explorers filtrados por misión
     static getExplorersByMission(mission) {
 
         const explorers = Reader.readJsonFile("./explorers.json");
         return ExplorerService.filterByMission(explorers, mission);
     }
 
-    static getExplorersUsernamesByMission(mission){
+    // metodo que regresa la lista de usernames de los explorers filtrados por la misión enviada.
+    static getExplorersUsernamesByMission(mission) {
+
         const explorers = this.getExplorersByMission(mission);
         return ExplorerService.getExplorersUsernames(explorers);
+    }
+
+    // metod que regresa la cantidad de explorers en la misión enviada
+    static getExplorersAmonutByMission(mission) {
+        
+        const explorers = this.getExplorersByMission(mission);
+        return ExplorerService.getExplorersAmount(explorers);
     }
 
 

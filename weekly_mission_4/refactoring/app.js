6d27c5ef14baf78c68e0ff521c9cd182ea7ccb93
ricardo55 <pegassoric@hotmail.com>
2022-06-30 19:35:15 -0500

    Agregue la case FizzBuzz Service para hacer POO

--- weekly_mission_4/refactoring/app.js
+++ weekly_mission_4/refactoring/app.js
@@ -70,4 +70,29 @@ const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) => assign
 //------
 
 const Reader = require("./lib/utils/Reader");
+const ExplorerService = require('./lib/services/ExplorerService')
+const FizzbuzzService = require('./lib/services/FizzbuzzService')
+
 const explorers2 = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
+
+// Clase anterior con la que obtenemos los explorers
+//const Reader = require("./lib/utils/Reader");
+
+// Aplicación del ExplorerService sobre la lista de explorers
+console.log(ExplorerService.filterByMission(explorers2, "node"));
+console.log(ExplorerService.getAmountOfExplorersByMission(explorers2, "node"));
+console.log(ExplorerService.getExplorersUsernamesByMission(explorers2, "node"));
+
+
+
+const explorer1 = {name: "Explorer1", score: 1}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer1)) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer3)) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer5)) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer15)) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}

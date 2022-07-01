7bc65d27546b4dac2a5dfba2e18cf0ca4cf3ce30
ricardo55 <pegassoric@hotmail.com>
2022-07-01 18:56:01 -0500

    Corri el lintern para arreglar el codigo

--- weekly_mission_4/refactoring/test/controllers/ExplorerController.test.js
+++ weekly_mission_4/refactoring/test/controllers/ExplorerController.test.js
@@ -23,14 +23,14 @@ describe("Test for ExplorerController",()=> {
 
 describe("Test service ExplorerController", ()=>{
     test("1.Probando metodo static getExplorersByMission, que recibe lista de explorers", ()=>{
-        const getExplorers=ExplorerController.getExplorersByMission("node")
-        expect(getExplorers).toBeDefined()
-    })
+        const getExplorers=ExplorerController.getExplorersByMission("node");
+        expect(getExplorers).toBeDefined();
+    });
 
     test("2.Test service metodo ExplorerController getExplorersUsernamesByMission", ()=>{
-        const getExplorersUsernamesByMission= ExplorerController.getExplorersUsernamesByMission("node")
-        expect(getExplorersUsernamesByMission).toBeDefined()
-    })
+        const getExplorersUsernamesByMission= ExplorerController.getExplorersUsernamesByMission("node");
+        expect(getExplorersUsernamesByMission).toBeDefined();
+    });
 
     // test ("3.Test service metodo ExplorerControoller getExplorersAmonutByMission(mission)", ()=>{
     //     const getAmountOfExplorersByMission= ExplorerController.getAmountOfExplorersByMission("node")

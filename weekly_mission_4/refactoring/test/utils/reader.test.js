83cbfd1a0a0e80493e0409506a1c3433ba2f2d6f
ricardo55 <pegassoric@hotmail.com>
2022-07-01 12:23:00 -0500

    Agregue pruebas para reader

---
+++ weekly_mission_4/refactoring/test/utils/reader.test.js
@@ -0,0 +1,15 @@
+const Reader = require("../../lib/utils/Reader");
+
+describe("Test for Reader", () => {
+
+    test("1. Obtain qty list of explorers", () => {
+        const explorers = Reader.readJsonFile("./explorers.json");
+        expect(explorers.length).toBe(15);
+    });
+
+    test("2. leer archivo Json que no existe", () => {
+        const explorers = Reader.readJsonFile("./explorers.json");
+        expect(explorers).toBe(explorers)
+    });
+
+});
